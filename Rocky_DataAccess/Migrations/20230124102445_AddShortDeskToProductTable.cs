fab492e3af8c167a261a9fef96d30ad9e33300df
User_Name <rostik319@mail.ru>
2023-05-18 19:50:46 +0700

    Preparing to deploy to Azure and Migrations bug fix

---
+++ Rocky_DataAccess/Migrations/20230124102445_AddShortDeskToProductTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky_DataAccess.Data.Migrations
+{
+    public partial class AddShortDeskToProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDesc",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortDesc",
+                table: "Product");
+        }
+    }
+}
--- Rocky_DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Rocky_DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rocky_DataAccess.Data;
 
-namespace Rocky_DataAccess.Migrations
+namespace Rocky_DataAccess.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
